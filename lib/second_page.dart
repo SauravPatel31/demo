ef000b22e45269f19738d2d7d6b0a6e0b60949d3
Saurav <sauravp3103@gmail.com>
2024-07-30 22:30:08 +0530

    second page in first commit

---
+++ lib/second_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
