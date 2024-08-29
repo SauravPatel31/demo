d5a3044dab70f579d83a6e25405f7d0dfbb4744a
Saurav <sauravp3103@gmail.com>
2024-08-29 10:40:12 +0530

    add appbar and

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      ///This is tital..
+      ///This isda
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
